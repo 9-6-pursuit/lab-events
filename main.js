69ce8cbb972c1873d7b30012d2550853d2d88aa4
Sabri Mohiuddin <you@example.com>
2023-03-19 12:23:05 -0400

    Events Lab_sabri

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 // Do not change the code below.
 const main = document.querySelector("main");
 for (let i = 0; i < 100; i++) {
@@ -7,3 +10,31 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+
+
+const currentColor  = document.querySelector("#current-color");
+const cells         = document.querySelectorAll(".cell");
+const paletteColors = document.querySelectorAll("#palette .color");
+
+// Set initial current color
+let currentBackgroundColor = "white";
+    currentColor.style.backgroundColor = currentBackgroundColor;
+
+// Change current color on palette click
+paletteColors.forEach((color) => {
+  color.addEventListener("click", () => {
+    currentBackgroundColor = color.style.backgroundColor;
+    currentColor.style.backgroundColor = currentBackgroundColor;
+  });
+});
+
+// Fill cell with current color on cell click
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    cell.style.backgroundColor = currentBackgroundColor;
+  });
+});
+
+
+
