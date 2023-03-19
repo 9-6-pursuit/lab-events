04385c82a2546af45e07a210ed219e0e40621369
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-03-19 12:14:10 -0400

    Done

--- main.js
+++ main.js
@@ -7,3 +7,33 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let colorButtons = document.querySelectorAll(".color")
+let currentColorSquare = document.querySelector("#current-color")
+         // ONE COLOR SELECTED
+// colorButtons[0].addEventListener("click", (event) => {
+//     // console.log(event.target.style.background)
+//     currentColorSquare.style.background = event.target.style.background
+//   }) 
+
+// LOOPS THROUGH EVERY COLOR
+
+// for (const colorButton of colorButtons){
+//   colorButton.addEventListener("click", (event) => {
+//   currentColorSquare.style.background = event.target.style.background
+//   })} 
+  colorButtons.forEach(colorButton =>
+    colorButton.addEventListener("click", (event) => {
+    currentColorSquare.style.background = event.target.style.background
+  })) //
+
+let blankCells = document.querySelector(".cell")
+
+for (const colorCell of blankCells){
+    colorCell.addEventListener("click", (event) => {
+      colorCell.style.background = currentColorSquare.style.background
+})}
+
+// blankCells.forEach(colorCell => {
+//     colorCell.addEventListener("mouseenter", (event) => {
+//       colorCell.style.background = currentColorSquare.style.background
+//   })}) //
