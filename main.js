ae0a0757897c64a49e7f821418aa0358891d0039
fintheehuman <finessepoole@pursuit.org>
2023-03-20 16:25:53 -0400

    just in the nick of time.

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+let colorButton = document.querySelectorAll(".color")
+
+let currentColor = document.querySelector('#current-color')
+
+for (const colors of colorButton) {
+  colors.addEventListener('click',  (event) => {
+    currentColor.style.background = event.target.style.background
+  })
+}
+
+let cells = document.querySelectorAll(".cell")
+
+for (const cell of cells) {
+  cell.addEventListener('click', () => {
+    cell.style.background = currentColor.style.background
+  })
+}
