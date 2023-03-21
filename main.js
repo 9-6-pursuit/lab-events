bd0c1babfe40c7a785d5a57a644128ff33bc635b
Lakisha J <lakishajohnson@pursuit.org>
2023-03-21 17:54:18 -0400

    Added more colors

--- main.js
+++ main.js
@@ -7,17 +7,6 @@ for (let i = 0; i < 100; i++) {
 }
 // You may write your code here!
 
-//When a color is clicked in the #palette, the #current-color element should have its background changed to that color
-let colors = document.querySelectorAll(".color")
-//console.log(colors)
-let currColor = document.querySelector("#current-color")
-colors.forEach((color) => { 
-  //console.log(color)
-  color.addEventListener("mousedown", (event) => {
-    currColor.style.background = event.target.style.background
-    // currColor.style.background = color.style.background
-  })
-})
 //When you click on a .cell, its background should change to match the background of #current-color
     let cells = document.querySelectorAll(".cell")
     //console.log(cells)
@@ -25,8 +14,8 @@ colors.forEach((color) => {
       //console.log(cell)
       cell.addEventListener("mousedown", () => {
       cell.style.background = currColor.style.background
-    })
-  })
+    });
+  });
 //COLORS USES EVENT AS A PARAM; CELLS DOES NOT BUT BOTH LISTENERS WORK
 
 //Create a button that, when clicked, resets all cells so that they all have a background of white
@@ -43,12 +32,31 @@ document.querySelector("#canvas").append(anotherButton);
 anotherButton.addEventListener("click", () => {
   cells.forEach((cell) => (cell.style.background = currColor.style.background));
 });
-//Create a button with a new color
+//Create a button with a new color(s)
 const addColor = document.createElement('div');
 addColor.setAttribute("class", "color")
 addColor.style.background = 'lightblue';
 //console.log(addColor.style.background)
 document.querySelector('#palette').append(addColor);
 
+let otherColors = ['#ff0000', 'rgb(165, 42, 42)', 'rgb(255, 192, 203)', 'rgb(230, 230, 250)', 'rgb(0, 128, 128)']
+for (const color of otherColors) {
+  const div = document.createElement("div")
+  div.classList.add("color")
+  div.style.background = color
+  // palette.append(div)
+  document.querySelector("#palette").append(div)
+};
+//When a color is clicked in the #palette, the #current-color element should have its background changed to that color
+  let colors = document.querySelectorAll(".color")
+  //console.log(colors)
+  let currColor = document.querySelector("#current-color")
+  colors.forEach((color) => { 
+    //console.log(color)
+    color.addEventListener("mousedown", (event) => {
+      currColor.style.background = event.target.style.background
+      // currColor.style.background = color.style.background
+    })
+  });
 
 
